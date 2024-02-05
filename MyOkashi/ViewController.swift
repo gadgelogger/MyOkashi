570ed8c6427bd0bf05a8dc317749afdeecae49e8
gadgelogger <sarayou273142910@gmail.com>
2024-02-05 21:19:53 +0900

    Initial Commit

---
+++ MyOkashi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyOkashi
+//
+//  Created by gadgelogger on 2024/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
